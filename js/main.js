ec4ad82f32d8454934ac737df2bbf1a50f5f206a
Brian Luong <bhluong1codes@gmail.com>
2021-12-01 22:09:03 -0800

    Listen for input events on photoUrl

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 /* global data */
 /* exported data */
+
+var $photoUrl = document.querySelector('#imgurl');
+var $img = document.querySelector('img');
+
+$photoUrl.addEventListener('input', function (event) {
+  if ($photoUrl.value === '' || !$photoUrl.value.includes('http')) {
+    $img.src = 'images/placeholder-image-square.jpg';
+  } else {
+    $img.src = $photoUrl.value;
+  }
+});
