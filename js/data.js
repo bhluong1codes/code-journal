569f773c6af3a882a221233ad7e4ca1e3aadec5c
Brian Luong <bhluong1codes@gmail.com>
2021-12-06 13:10:12 -0800

    Updated JS files

--- js/data.js
+++ js/data.js
@@ -7,36 +7,6 @@ var data = {
   nextEntryId: 1
 };
 
-var $photoUrl = document.querySelector('#imgurl');
-var $img = document.querySelector('img');
-var $form = document.querySelector('form');
-var $title = document.querySelector('#title');
-var $notes = document.querySelector('#notes');
-
-$photoUrl.addEventListener('input', function (event) {
-  if ($photoUrl.value === '') {
-    $img.src = 'images/placeholder-image-square.jpg';
-  } else {
-    $img.src = $photoUrl.value;
-  }
-});
-
-$form.addEventListener('submit', function (event) {
-  event.preventDefault();
-  var titleValue = $title.value;
-  var photoUrlValue = $photoUrl.value;
-  var notesValue = $notes.value;
-
-  var entry = {
-    title: titleValue,
-    photoUrl: photoUrlValue,
-    notes: notesValue,
-    entryId: data.nextEntryId
-  };
-  data.nextEntryId++;
-  data.entries.unshift(entry);
-});
-
 window.addEventListener('beforeunload', function (event) {
   var entriesJSON = JSON.stringify(data);
   localStorage.setItem('local-storage', entriesJSON);
--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 /* global data */
 /* exported data */
+
+var $photoUrl = document.querySelector('#imgurl');
 var $img = document.querySelector('img');
 var $form = document.querySelector('form');
+var $title = document.querySelector('#title');
+var $notes = document.querySelector('#notes');
+
+$photoUrl.addEventListener('input', function (event) {
+  if ($photoUrl.value === '') {
+    $img.src = 'images/placeholder-image-square.jpg';
+  } else {
+    $img.src = $photoUrl.value;
+  }
+});
 
 $form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var titleValue = $title.value;
+  var photoUrlValue = $photoUrl.value;
+  var notesValue = $notes.value;
+
+  var entry = {
+    title: titleValue,
+    photoUrl: photoUrlValue,
+    notes: notesValue,
+    entryId: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.unshift(entry);
   $form.reset();
   $img.src = 'images/placeholder-image-square.jpg';
 });
