a82b06bd064badf1f76f612dcf0a9d6ac3f4bada
Brian Luong <bhluong1codes@gmail.com>
2021-12-06 22:37:46 -0800

    Removed the example HTML entries

--- js/main.js
+++ js/main.js
@@ -32,3 +32,46 @@ $form.addEventListener('submit', function (event) {
   $form.reset();
   $img.src = 'images/placeholder-image-square.jpg';
 });
+
+// <li class="row">
+//   <div class="column-half">
+//     <div class="img-container">
+//       <img src="data.photoUrl" alt="">
+//     </div>
+//   </div>
+//   <div class="column-half">
+//     <h3>
+//       data.title;
+//     </h3>
+//     <p>
+//       data.notes;
+//     </p>
+//   </div>
+// </li>
+
+function createEntry(event) {
+  var $entries = document.querySelector('.entries-list');
+  for (var i = 0; i < data.entries.length; i++) {
+    var $entry = document.createElement('li');
+    $entry.setAttribute('class', 'row');
+    var $divCol = document.createElement('div');
+    $divCol.setAttribute('class', 'column-half');
+    var $divCol2 = document.createElement('div');
+    $divCol2.setAttribute('class', 'column-half');
+    var $imgContainer = document.createElement('div');
+    $imgContainer.setAttribute('class', 'img-container');
+    var $img = document.createElement('img');
+    $img.setAttribute('src', data.entries[i].photoUrl);
+    var $h3 = document.createElement('h3');
+    $h3.textContent = data.entries[i].title;
+    var $p = document.createElement('p');
+    $p.textContent = data.entries[i].notes;
+
+    $entry.appendChild($divCol).appendChild($imgContainer).appendChild($img);
+    $entry.appendChild($divCol2).appendChild($h3);
+    $divCol2.appendChild($p);
+    $entries.appendChild($entry);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', createEntry);
